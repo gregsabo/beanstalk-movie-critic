23da3d2f2f5938abfeb2951323802b1a60ef7a22
Greg Sabo <gregsabo@stripe.com>
2016-01-24 22:43:56 -0800

    Form works

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import Helmet from 'react-helmet';
-import reduxForm from 'redux-form';
+import { reduxForm } from 'redux-form';
 import * as criticActions from 'redux/modules/critic';
 import { Input, ButtonInput } from 'react-bootstrap';
 import { pushState } from 'redux-router';
@@ -8,28 +8,29 @@ import { pushState } from 'redux-router';
 @reduxForm({
   form: 'critic',
   fields: ['movieName']
-}, state => ({
+}, () => ({
 }), criticActions)
 export default class Home extends Component {
   static propTypes = {
-    movieNameField: PropTypes.object,
     submitMovie: PropTypes.func,
+    dispatch: PropTypes.func,
+    fields: PropTypes.object
   };
 
   render() {
+    const movieNameField = this.props.fields.movieName;
     return (
-      const movieField = this.props.fields.movieNameField;
       <div className="container-fluid">
         <Helmet title="Home"/>
         <h1>Automatic Movie Critic</h1>
         <p>What is your favorite movie?</p>
         <form onSubmit={event => {
           event.preventDefault();
-          this.props.submitMovie(movieField.value);
+          this.props.submitMovie(movieNameField.value);
           this.props.dispatch(pushState(null, 'critic'));
         }}>
-          <Input type="text" label="Movie Name" {...movieField} />
-          <ButtonInput type="submit" />
+          <Input type="text" label="Movie Name" {...movieNameField} />
+          <ButtonInput type="submit" bsStyle="primary" />
         </form>
       </div>
     );
